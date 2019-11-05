0cd5aa931c76b1840cbd2c02b5e091954156af2f
Davide Guida <davide_guida@dell.com>
2019-11-05 14:11:17 +0100

    refactoring

--- WebApiWithBackgroundWorker.Publisher/RabbitPublisher.cs
+++ WebApiWithBackgroundWorker.Publisher/RabbitPublisher.cs
@@ -6,35 +6,40 @@ using System.Text;
 
 namespace WebApiWithBackgroundWorker.Publisher
 {
-    public class RabbitPublisher
+    public class RabbitPublisher : IDisposable
     {
-        private readonly IBusConnection _connection;
-        private const string ExchangeName = "messages";
+        private const string ExchangeName = "messages"; 
+        
+        private readonly IBusConnection _connection;        
+        private readonly IModel _channel;
+        private readonly IBasicProperties _properties;
 
-        public RabbitPublisher(IBusConnection persistentConnection)
+        public RabbitPublisher(IBusConnection connection)
         {
-            _connection = persistentConnection ?? throw new ArgumentNullException(nameof(persistentConnection));            
+            _connection = connection ?? throw new ArgumentNullException(nameof(connection));
+            _channel = _connection.CreateChannel();
+            _channel.ExchangeDeclare(exchange: ExchangeName, type: ExchangeType.Fanout);
+            _properties = _channel.CreateBasicProperties();
         }
 
         public void Publish(Message message)
-        {           
-            using (var channel = _connection.CreateChannel())
-            {
-                channel.ExchangeDeclare(exchange: ExchangeName, type: ExchangeType.Fanout);
+        {
+            var jsonData = JsonSerializer.Serialize(message);
 
-                var jsonData = JsonSerializer.Serialize(message);
+            var body = Encoding.UTF8.GetBytes(jsonData);            
 
-                var body = Encoding.UTF8.GetBytes(jsonData);
+            _channel.BasicPublish(
+                exchange: ExchangeName,
+                routingKey: string.Empty,
+                mandatory: true,
+                basicProperties: _properties,
+                body: body);
+        }
 
-                var properties = channel.CreateBasicProperties();
-
-                channel.BasicPublish(
-                    exchange: ExchangeName,
-                    routingKey: string.Empty,
-                    mandatory: true,
-                    basicProperties: properties,
-                    body: body);
-            }
+        public void Dispose()
+        {
+            _channel?.Dispose();
+            _channel = null;
         }
     }
 }
